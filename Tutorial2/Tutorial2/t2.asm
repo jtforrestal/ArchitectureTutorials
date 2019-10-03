6ac54e29fe98bf9003957d8ffb8afd602c219cea
Jack Forrestal <jforrest@tcd.ie>
2019-10-03 21:04:16 +0100

    Code Samples constructed, prior to testing

--- Tutorial2/Tutorial2/t2.asm
+++ Tutorial2/Tutorial2/t2.asm
@@ -1,7 +1,9 @@
 option casemap:none
 .data
 public	g
-g		DWORD	4
+g		QWORD	4
+
+qxp		db	'"a = %I64d b = %I64d c = %I64d d = %I64d e = %I64d sum = %I64d', 0AH,00H
 .code
 
 public	minX64
@@ -16,4 +18,74 @@ bnotlv:	cmp		rax,r8
 cnotlv:	ret
 
 
+
+public	pX64
+
+pX64:	push	r9	;store our inputs k and l
+		push	r8
+
+		mov		r8,rdx	;move our original parameters into appropriate registers for min call
+		mov		rdx,rcx
+		mov		rcx,#g
+
+		sub		rsp,32	;allocate 32 bits of sadow space
+
+		call	min
+
+		add		rsp,32	;temporarily deallocating shadow space to retrieve k and l
+		mov		rcx,rax
+		pop		rdx
+		pop		r8
+
+		sub		rsp,32	;alloc
+
+		call	min
+
+		add		rsp,32	;dalloc
+		ret				;min call returns result to rax anyways so we can return again
+
+
+public	gcdX64
+
+gcdX64:	mov		rax,rcx		;Push parameters into appropriate registers
+		mov		rbx,rdx
+
+		cmp		rbx,0		;Check if param b is eqaul to 0
+		je		reta
+		cqo
+		idiv	rbx			;Modulo result stored in rdx, so it wil already be second parameter for recursive call
+		mov		rcx,rbx		;move b to be first param
+
+		sub		rsp,32		;allocating shadow space
+		call	gcdX64
+		add		rsp,32		;all varaible stored in registers so nothing to be cleaned up before deallocating chadow space
+
+reta:	ret					;First paramter already been pushed to rax, so if there is no recursion we still get teh correct result	
+		
+public	qX64
+
+qX64:	mov		rax,rcx		;Add each parameter together store sum in rax
+		add		rax,rdx
+		add		rax,r8
+		add		rax,r9
+
+		pop		rbx
+
+		add		rax,rbx
+
+		push	r9			;pushing our excess variables to teh stack
+		push	rbx
+		push	rax
+
+		mov		r9,r8		
+		mov		r8,rdx	
+		mov		rdx,rcx		
+		lea		rcx,qxp		;push the string for printf to first param
+
+
+		sub		rsp,32		;Alocating shadow space
+		call	printf
+		add		rsp,32		;Deallocating Shadow
+		pop		rax
+		ret
 		end
--- Tutorial2/Tutorial2/t2.h
+++ Tutorial2/Tutorial2/t2.h
@@ -14,4 +14,7 @@
 
 
 extern "C" _int64 minX64(_int64);	
+extern "C" _int64 pX64(_int64, _int64, _int64, _int64);
+extern "C" _int64	gcdX64(_int64, _int64);
+extern "C" _int64 qX64(_int64, _int64, _int64, _int64, _int64)
 // eof
