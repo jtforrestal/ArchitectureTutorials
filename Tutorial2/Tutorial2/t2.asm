61fadedaa6efbd99ba41d9bfc50108011595006d
Jack Forrestal <jforrest@tcd.ie>
2019-10-07 02:55:03 +0100

    Actually understand shadow space, updated p and finished q

--- Tutorial2/Tutorial2/t2.asm
+++ Tutorial2/Tutorial2/t2.asm
@@ -28,27 +28,24 @@ cnotlv:	ret
 
 public	pX64
 
-pX64:	push	r9	;store our inputs k and l
-		push	r8
+pX64:	
+		sub		rsp,48	;allocate 32 bits of sadow space + two variables
+		mov		[rsp+40],r9	;store our inputs k and l
+		mov		[rsp+32],r8
 
 		mov		r8,rdx	;move our original parameters into appropriate registers for min call
 		mov		rdx,rcx
 		mov		rcx,g
 
-		sub		rsp,32	;allocate 32 bits of sadow space
-
 		call	minX64
 
-		add		rsp,32	;temporarily deallocating shadow space to retrieve k and l
 		mov		rcx,rax
-		pop		rdx
-		pop		r8
-
-		sub		rsp,32	;alloc
+		mov		rdx,[rsp+40]
+		mov		r8,	[rsp+32]
 
 		call	minX64
 
-		add		rsp,32	;dalloc
+		add		rsp,48	;deallocating shadow space and variable space
 		ret				;min call returns result to rax anyways so we can return again
 
 
@@ -63,7 +60,7 @@ gcdX64:	mov		rax,rcx		;Push parameters into appropriate registers
 		idiv	rbx			;Modulo result stored in rdx, so it wil already be second parameter for recursive call
 		mov		rcx,rbx		;move b to be first param
 
-		sub		rsp,32		;allocating shadow space
+		sub		rsp,32		;allocating shadow space, no variable on stack, so only need 32bytes
 		call	gcdX64
 		add		rsp,32		;all varaible stored in registers so nothing to be cleaned up before deallocating chadow space
 
@@ -71,28 +68,27 @@ reta:	ret					;First paramter already been pushed to rax, so if there is no recu
 		
 public	qX64
 
-qX64:	mov		rax,rcx		;Add each parameter together store sum in rax
+qX64:	
+		mov		rbx,[rsp+40]	;access the e input parameter from above shadow space
+		sub		rsp,56	;Creating shadow space and allocating space for variables
+		
+		mov		rax,rcx		;adding all the inputs
+		add		rax,rbx
 		add		rax,rdx
 		add		rax,r8
 		add		rax,r9
 
-		pop		rbx
-
-		add		rax,rbx
-
-		push	r9			;pushing our excess variables to teh stack
-		push	rbx
-		push	rax
-
-		mov		r9,r8		
-		mov		r8,rdx	
-		mov		rdx,rcx		
-		lea		rcx,qxp		;push the string for printf to first param
-
-
-		sub		rsp,32		;Alocating shadow space
+		mov		[rsp+48],rax		;placing paramters 5,6,7 for printf above the shadow space
+		mov		[rsp+40],rbx
+		mov		[rsp+32],r9
+		
+		mov		r9,r8		;moving the bottom four parameters into the correct registers
+		mov		r8,rdx
+		mov		rdx,rcx
+		lea		rcx,qxp
 		call	printf
-		add		rsp,32		;Deallocating Shadow
-		pop		rax
+		mov		rax,[rsp+48]	; collecting out sum 
+		add		rsp,56			;de allocating the shadow space
+
 		ret
 		end
--- Tutorial2/Tutorial2/t2.h
+++ Tutorial2/Tutorial2/t2.h
@@ -15,6 +15,6 @@
 extern "C" _int64 g;
 extern "C" _int64 minX64(_int64,_int64, _int64);	
 extern "C" _int64 pX64(_int64, _int64, _int64, _int64);
-extern "C" _int64	gcdX64(_int64, _int64);
+extern "C" _int64 gcdX64(_int64, _int64);
 extern "C" _int64 qX64(_int64, _int64, _int64, _int64, _int64);
 // eof
